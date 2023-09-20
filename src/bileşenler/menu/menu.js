32e2c0f42714c0f0b8fb0815184e9f990af67ef0
Alişan Karababa <alisankarababa@gmail.com>
2023-09-20 13:08:25 +0300

    gorevler tamamlandi, testten gecti, data baska bir modulden import edildi

--- src/bileşenler/menu/menu.js
+++ src/bileşenler/menu/menu.js
@@ -27,10 +27,43 @@ let menuElemanlari = [
   Adım 3: Hala fonksiyon içindeyiz, DOM'dan menü düğmesini seçin (`index.html` içinde `menu-button` sınıfına (class) sahip öğe).
 
   Adım 4: Menü butonuna bir `click` event dinleyicisi ekleyin. Butona her tıklanıldığında div.menu classına `menu--open`('menu' class'ına sahip olan div) ekleyip/silecek(toogle).
-  
-  İPUCU: Javascript'te toogle metotları bir öğeyi bir elemana ekleyip/çıkarmaya yarar. Örnek olarak bir öğeye tıklandığında bir class toogle'ı kullanıldığında, o öğreye tıklandığında eğer o class ismi öğede yoksa ekler, eğer o class ismi öğede varsa siler. Toogle - aç kapa manasına da gelir.
 
-  Adım 5: Oluştrulan div.menu 'yü döndürmeyi unutmayın.
+   İPUCU: Javascript'te toogle metotları bir öğeyi bir elemana ekleyip/çıkarmaya yarar. Örnek olarak bir öğeye tıklandığında bir class toogle'ı kullanıldığında, o öğreye tıklandığında eğer o class ismi öğede yoksa ekler, eğer o class ismi öğede varsa siler. Toogle - aç kapa manasına da gelir.
+
+
+   Adım 5: Oluştrulan div.menu 'yü döndürmeyi unutmayın.
 
   Adım 6: 'menuYapici' fonksiyonunu ve 'menuElemanlari' dizisini kullanarak menüyü oluşturun, ve döndürülen menüyü header'e ekleyin.
-*/
+
+  */
+
+  function menuYapici(arrMenuItems)
+  {
+    const menuDiv = document.createElement("div");
+    menuDiv.classList.add("menu");
+      const ul = document.createElement("ul");
+      menuDiv.append(ul);
+        arrMenuItems.forEach(menuItem => {
+          const li = document.createElement("li");
+          li.textContent = menuItem;
+          ul.append(li);
+        });
+
+    const menuButton = document.querySelector(".menu-button");
+    menuButton.addEventListener("click", (e) => {
+      document.querySelector("div.menu").classList.toggle("menu--open");
+    })
+    
+    return menuDiv;
+  }
+    
+  document.querySelector(".header").append(menuYapici(menuElemanlari));
+
+  
+
+  
+
+  
+ 
+
+  
